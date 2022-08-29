543cc2af83370c877aac1333fe5a98480f267809
RubenFigueiredo98 <ruben.barroso.figueiredo@gmail.com>
2022-08-29 11:24:50 +0100

    final

--- ex00/ft_strcmp.c
+++ ex00/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: ruben <ruben@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 10:15:19 by ruben             #+#    #+#             */
-/*   Updated: 2022/08/29 11:00:18 by ruben            ###   ########.fr       */
+/*   Updated: 2022/08/29 11:23:38 by ruben            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,19 @@ int ft_strcmp(char *s1, char *s2);
 
 int main(void)
 {
-    char s1[] = "Hello";
-    char s2[] = "Hello";
+    char s1[] = "Hall9v";
+    char s2[] = "Hell0dfd";
     printf("%d", ft_strcmp(s1, s2));
 }
 
 int ft_strcmp(char *s1, char *s2)
 {
-    int i;
+    unsigned int i;
 
     i = 0;
-    while(s1[i] != '\0')
-    {   
-        if (s1[i] != s2[i])
-        {
-            if(s1[i] > s2[i])
-            {
-                return(1);
-            }
-            else if(s1[i] < s2[i])
-            {
-                return(-1);
-            }
-        }
+    while (s1[i] == s2[i] && (s1[i] != '\0' && s2[i] != '\0'))
+    {
         i++;
     }
-    return(0);
+    return(s1[i] - s2[i]);
 }
